a7eb4d9947ea89a1da9e9cb5bc3963bf79fa3250
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-03-26 00:25:14 -0400

    added javascript to load inital set of questions

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: '1998Tiger3!',
   database: 'employee_db',
 });
 const userQ = [{
   type: 'list',
-  name: '',
+  name: 'startQ',
   message: 'How would you like to proceed?',
   choices: ['View Departments', 'View Roles', 'View Employees', 'New Department', 'New Role', 'New Employee', 'Update Employee',],
 }];
+function question() {
+  inquirer.prompt(userQ).then((userA) => {
+    console.log(userA.startQ);
+    let userInput = userA.startQ;
+    if (userInput === 'View Departments') {viewDepartments();}
+    if (userInput === 'View Roles') {viewRoles();}
+    if (userInput === 'View Employees') {viewEmployees();}
+    if (userInput === 'New Department') {newDepartment();}
+    if (userInput === 'New Role') {newRole();}
+    if (userInput === 'New Employee') {newEmployee();}
+  });
+}
+question();
