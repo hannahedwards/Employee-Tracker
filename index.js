1d95498bf72f9e6cb4c94ad05ad48e4db850a4d0
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-03-26 22:18:52 -0400

    added javascript for adding new departments,roles,employees

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'PASSWORD',
   database: 'employee_db',
 });
 const userQ = [{
   type: 'list',
   name: 'startQ',
   message: 'How would you like to proceed?',
-  choices: ['View Departments', 'View Roles', 'View Employees', 'New Department', 'New Role', 'New Employee', 'Update Employee',],
+  choices: ['View Departments', 'View Roles', 'View Employees', 'New Department', 'New Role', 'New Employee',],
 }];
 function question() {
   inquirer.prompt(userQ).then((userA) => {
@@ -31,20 +31,107 @@ const viewDepartments = () => {
     question();
   });
 };
-
 const viewRoles = () => {
   db.query('SELECT * FROM roles JOIN department ON roles.department_id = department.id;',
     function (err, results) {
       console.table(results);
       question();
-    }
-  );
-};
+    });
+  };
 const viewEmployees = () => {
   db.query( "SELECT * FROM employee JOIN roles ON employee.role_id = roles.id JOIN department ON roles.department_id = department.id;",
     function (err, results) {
       console.table(results);
       question();
-    }
-  );
+    });
+};
+const newDepartment = () => {
+  department = [
+    { type: 'input',
+      name: 'departrment',
+      message: 'What department would you like to add?',
+    },
+  ];
+  inquirer.prompt(department).then((userA) => {
+    console.log(userA.departrment);
+    const add = `INSERT INTO department (department_name) VALUES (?)`;
+    const place = [userA.departrment];
+    db.query(add, place, (err, result) => {
+      if (err) {
+        console.log('Try Again');
+      } else {
+        console.log('Nice!');
+      }
+      userA, (err) => (err ? console.log(err) : console.log('Nice!'));
+      question();
+    });
+  });
+};
+const newRole = () => {
+  role = [
+    { type: 'input',
+      name: "role",
+      message: 'What role would you like to add?',
+    },
+    { type: 'number',
+      name: 'salary',
+      message: 'What would you like to salary to be set at?',
+    },
+    { type: 'list',
+      name: 'department',
+      message: 'What departmet is this role in?',
+      choices: ['Engineering', 'Project Managment'],
+    },
+  ];
+  inquirer.prompt(role).then((userA) => {
+    const add = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+    const place = [userA.role, userA.salary, userA.department];
+    db.query(add, place, (err, result) => {
+      if (err) {
+        console.log('Try Again');
+      } else {
+        console.log('Nice!');
+      }
+      userA, (err) => (err ? console.log(err) : console.log("Nice!"));
+      question();
+    });
+  });
+};
+const newEmployee = () => {
+  employee = [
+    { type: 'input',
+      name: 'firstName',
+      message: 'New employees first name?',
+    },
+    { type: 'input',
+      name: 'lastName',
+      message: 'New employees last name?',
+    },
+    { type: 'list',
+      name: 'role_Id',
+      message:'What role is your new employee taking on?',
+      choices: ['Senior Dev', 'Junior Dev', 'UX/UI Designer', 'IT',],
+    },
+    { type: 'list',
+      name: 'manager_Id',
+      message: 'Whats your new employees manager ID?',
+      choices: ['001', '002',],
+    },
+  ];
+  inquirer.prompt(employee).then((userA) => {
+    const add = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+    const place = [
+      userA.firstName,
+      userA.lastName,
+      userA.role_Id,
+      userA.manager_Id,
+    ];
+    db.query(add, place, (err, result) => {
+      if (err) {
+        console.log('Try Again!');
+      } else { console.log('Nice!'); }
+      userA, (err) => (err ? console.log(err) : console.log('Nice!'));
+      question();
+    });
+  });
 };
