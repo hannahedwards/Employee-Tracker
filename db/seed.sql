04d62e8eb4e202d300fa9863326c26a4255f11ba
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-03-26 21:26:59 -0400

    update to seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -3,13 +3,13 @@ VALUES ('Engineering'),
 ('Project Management');
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ('Senior Dev', 180000, 012),
-('Junior Dev', 75000, 012),
-('UX/UI Designer', 75000, 023),
-('IT', 100000, 023);
+VALUES ('Senior Dev', 180000, 1),
+('Junior Dev', 75000, 1),
+('UX/UI Designer', 75000, 2),
+('IT', 100000, 2);
 
  INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Stan', 'Smith', 321, 123),
-('Jill', 'Tomas', 432, 123),
-('Kevin', 'Elliot', 543, 456),
-('Becca', 'White', 654, 456); 
+VALUES ('Stan', 'Smith', 1, 0001),
+('Jill', 'Tomas', 2, 0001),
+('Kevin', 'Elliot', 3, 0002),
+('Becca', 'White', 4, 0002); 
